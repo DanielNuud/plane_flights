151f9400731b1b59cd76d5cb1753f09574027729
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-03-19 11:14:01 +0200

    update

--- src/main/java/daniel/nuud/plane_flights/config/SecurityConfig.java
+++ src/main/java/daniel/nuud/plane_flights/config/SecurityConfig.java
@@ -6,49 +6,36 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
-public class SecurityConfig extends WebSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request ->
+                request.requestMatchers("/styles/*", "/login", "/register").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .failureUrl("/login?error")
+                )
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public AuthenticationManager authenticationManager(
-            final AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
-
-    @Bean
-    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
-        return http.cors(withDefaults())
-                .csrf(withDefaults())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/login").permitAll()
-                        .requestMatchers("/register").permitAll()
-                        .requestMatchers("/css/**", "/js/**", "/images/**", "/static/**", "/webjars/**").permitAll()
-                        .requestMatchers("/v3/api-docs/**").permitAll()
-                        .requestMatchers("/swagger.html").permitAll()
-                        .requestMatchers("/swagger-ui/**").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .usernameParameter("email")
-                        .failureUrl("/login?loginError=true"))
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/login?logoutSuccess=true")
-                        .deleteCookies("JSESSIONID"))
-                .exceptionHandling(exception -> exception
-                        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login?loginRequired=true")))
-                .build();
-    }
-
 }
