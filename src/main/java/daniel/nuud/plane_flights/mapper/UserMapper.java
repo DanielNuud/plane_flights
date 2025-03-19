62804d65dfe0c7c4eec7fd818fd121d6cb29d6e1
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-03-19 11:13:37 +0200

    added passwordEncode mapping

--- src/main/java/daniel/nuud/plane_flights/mapper/UserMapper.java
+++ src/main/java/daniel/nuud/plane_flights/mapper/UserMapper.java
@@ -3,10 +3,14 @@ package daniel.nuud.plane_flights.mapper;
 import daniel.nuud.plane_flights.dto.RegistrationRequest;
 import daniel.nuud.plane_flights.dto.UserDTO;
 import daniel.nuud.plane_flights.model.User;
+import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
         componentModel = MappingConstants.ComponentModel.SPRING,
@@ -14,8 +18,19 @@ import org.mapstruct.ReportingPolicy;
 )
 public abstract class UserMapper {
 
+    @Autowired
+    protected PasswordEncoder passwordEncoder;
 
     public abstract UserDTO toDTO(User user);
+
     public abstract User fromDTO(UserDTO userDTO);
+
     public abstract User toEntity(RegistrationRequest registrationRequest);
+
+    @BeforeMapping
+    protected void encodePassword(RegistrationRequest registrationRequest, @MappingTarget User user) {
+        if (registrationRequest.getPassword() != null) {
+            user.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
+        }
+    }
 }
