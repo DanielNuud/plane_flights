8c589facf66688d883a95ea18020bdb27533ff1c
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-03-21 10:10:49 +0200

    added cashing

--- src/main/java/daniel/nuud/plane_flights/PlaneFlightsApplication.java
+++ src/main/java/daniel/nuud/plane_flights/PlaneFlightsApplication.java
@@ -2,8 +2,10 @@ package daniel.nuud.plane_flights;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PlaneFlightsApplication {
 
     public static void main(String[] args) {
--- src/main/java/daniel/nuud/plane_flights/config/Configuration.java
+++ src/main/java/daniel/nuud/plane_flights/config/Configuration.java
@@ -1,7 +1,12 @@
 package daniel.nuud.plane_flights.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.concurrent.TimeUnit;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
@@ -11,4 +16,15 @@ public class Configuration {
         return WebClient.builder();
     }
 
+    @Bean
+    public Caffeine caffeineConfig() {
+        return Caffeine.newBuilder().expireAfterWrite(1, TimeUnit.DAYS);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
 }
