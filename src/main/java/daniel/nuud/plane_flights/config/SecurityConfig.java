63bfe5d9759d369ce0361c460c2c6147c91b6161
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-03-22 10:15:14 +0200

    fix

--- src/main/java/daniel/nuud/plane_flights/config/SecurityConfig.java
+++ src/main/java/daniel/nuud/plane_flights/config/SecurityConfig.java
@@ -18,11 +18,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request ->
-                request.requestMatchers("/styles/*", "/login", "/register", "/flights").permitAll()
+                request.requestMatchers("/styles/*", "/login", "/register").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/flights", true)
+                        .defaultSuccessUrl("/", true)
                         .failureUrl("/login?error")
                 )
                 .logout(LogoutConfigurer::permitAll);
