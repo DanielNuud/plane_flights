db667e3296e47f3befae3c0d3bde16c670b8e860
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-03-22 15:20:13 +0200

    created AirportService

---
+++ src/main/java/daniel/nuud/plane_flights/service/AirportService.java
@@ -0,0 +1,20 @@
+package daniel.nuud.plane_flights.service;
+
+import daniel.nuud.plane_flights.model.Airport;
+import daniel.nuud.plane_flights.repository.AirportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AirportService {
+
+    @Autowired
+    private AirportRepository airportRepository;
+
+    public List<Airport> getAllAirports() {
+        return airportRepository.findAll();
+    }
+
+}
