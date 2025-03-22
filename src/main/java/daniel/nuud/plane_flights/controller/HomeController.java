9c88c63a9a335e5a4142218c7521b78474594aef
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-03-22 15:20:41 +0200

    update

--- src/main/java/daniel/nuud/plane_flights/controller/HomeController.java
+++ src/main/java/daniel/nuud/plane_flights/controller/HomeController.java
@@ -1,11 +1,11 @@
 package daniel.nuud.plane_flights.controller;
 
-import daniel.nuud.plane_flights.dto.api.AirportDataDTO;
 import daniel.nuud.plane_flights.dto.api.FlightDataDTO;
-import daniel.nuud.plane_flights.service.AirportApiService;
-import daniel.nuud.plane_flights.service.FlightApiService;
+import daniel.nuud.plane_flights.model.Airport;
+import daniel.nuud.plane_flights.service.AirportService;
+import daniel.nuud.plane_flights.service.api.AirportApiService;
+import daniel.nuud.plane_flights.service.api.FlightApiService;
 import daniel.nuud.plane_flights.service.FlightService;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,12 +20,18 @@ public class HomeController {
     private FlightApiService flightApiService;
 
     @Autowired
-    private AirportApiService airportApiService;
+    private AirportService airportService;
+
+    @Autowired
+    private FlightService flightService;
 
     @GetMapping("/")
     public String home(Model model) {
         List<FlightDataDTO> flights = flightApiService.getRealTimeFlights();
+        flightService.getFlights();
+        List<Airport> airports = airportService.getAllAirports();
         model.addAttribute("flights", flights);
+        model.addAttribute("airports", airports);
         return "home";
     }
 }
