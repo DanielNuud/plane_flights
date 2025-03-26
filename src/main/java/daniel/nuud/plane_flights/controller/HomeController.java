93b40f229052980052a59651c78ccf613695d146
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-03-26 21:41:48 +0200

    update

--- src/main/java/daniel/nuud/plane_flights/controller/HomeController.java
+++ src/main/java/daniel/nuud/plane_flights/controller/HomeController.java
@@ -2,6 +2,7 @@ package daniel.nuud.plane_flights.controller;
 
 import daniel.nuud.plane_flights.dto.api.FlightDataDTO;
 import daniel.nuud.plane_flights.model.Airport;
+import daniel.nuud.plane_flights.model.Flight;
 import daniel.nuud.plane_flights.service.AirportService;
 import daniel.nuud.plane_flights.service.api.FlightApiService;
 import daniel.nuud.plane_flights.service.FlightService;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -26,20 +28,19 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        List<FlightDataDTO> flights = flightApiService.getRealTimeFlights();
-        flightService.getFlights();
+//        List<FlightDataDTO> flights = flightApiService.getRealTimeFlights();
+//        flightService.getFlights();
         List<Airport> airports = airportService.getAllAirports();
 
-        flightService.updateFlightAssociations();
+//        flightService.updateFlightAssociations();
         List<String> departureCities = flightService.findDistinctDepartureCities();
         List<String> arrivalCities = flightService.findDistinctArrivalCities();
 
+        List<Flight> flightList = flightService.getAllFlightsWithAirlines();
+
         model.addAttribute("departureCities", departureCities);
         model.addAttribute("arrivalCities", arrivalCities);
-
-
-
-        model.addAttribute("flights", flights);
+        model.addAttribute("flights", flightList);
         model.addAttribute("airports", airports);
         return "main";
     }
